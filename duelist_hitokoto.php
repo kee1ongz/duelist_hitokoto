81db91763c3a80990021371d83791688640dc275
kee1ong <45457125+kee1ong@users.noreply.github.com>
2021-05-13 16:47:34 +0800

    Add files via upload
    Released v0.1.

---
+++ duelist_hitokoto.php
@@ -0,0 +1,26 @@
+<?php
+
+header('X-Powered-By:Kee1ongz');
+header('access-control-allow-origin:*');
+
+  $array=file('duel.txt');
+  //var_dump($array);
+  $rand=rand(0,12);
+  //var_dump($rand);
+  $string=$array[$rand];
+  //var_dump($string);
+  
+header('Content-Type: text/html; charset=UTF-8');
+    if ($_GET['code']==='js') {
+		 header('Content-type: application/x-javascript; charset=UTF-8');
+          echo "function duelist(){document.write(\"";
+          echo trim($string);
+          echo "\");}";
+		 }else{
+          //var_dump($string);
+          echo trim($string);
+
+          }
+	
+?>
+
